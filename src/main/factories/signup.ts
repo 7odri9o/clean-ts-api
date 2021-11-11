35a65fc7d7ad97afdd2d4d4777c5a78fc0a40d5a
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-11-11 13:38:31 +0000

    feat: move signup validation fields resposibility to a composite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,12 +1,12 @@
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
-import { Validator } from '../../data/usecases/validation/validator'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -14,8 +14,8 @@ export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbaddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const validation = new Validator()
-  const signUpController = new SignUpController(emailValidatorAdapter, dbaddAccount, validation)
+  const validationComposite = makeSignUpValidation()
+  const signUpController = new SignUpController(emailValidatorAdapter, dbaddAccount, validationComposite)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
