070033fe4b93808ba4d73c5332aee25bf0c5d05b
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-11-11 13:50:59 +0000

    refactor: move required field validation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,3 +1,5 @@
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { makeSignUpValidation } from './signup-validation'
 
@@ -6,6 +8,10 @@ jest.mock('../../presentation/helpers/validators/validation-composite')
 describe('SignUpValidation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
     makeSignUpValidation()
-    expect(ValidationComposite).toHaveBeenCalledWith([])
+    const validations: Validation[] = []
+    for (const field of ['name', 'email', 'password', 'confirmationPassword']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
