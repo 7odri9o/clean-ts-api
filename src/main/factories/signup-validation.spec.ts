570a63209fa2d84aef6bbeff2fc6137f486f514f
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-11-11 13:38:54 +0000

    test: move signup validation fields resposibility to a composite

---
+++ src/main/factories/signup-validation.spec.ts
@@ -0,0 +1,11 @@
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+import { makeSignUpValidation } from './signup-validation'
+
+jest.mock('../../presentation/helpers/validators/validation-composite')
+
+describe('SignUpValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeSignUpValidation()
+    expect(ValidationComposite).toHaveBeenCalledWith([])
+  })
+})
