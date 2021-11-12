03701a15ad9ed91764fad3a2891127eafca32832
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-11-12 23:27:52 +0000

    test: ensure AccountMongoRepository returns return null if loadByEmail fails

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -50,4 +50,10 @@ describe('Account Mongo Repository', () => {
     expect(account?.email).toBe('valid_email@email.com')
     expect(account?.password).toBe('valid_password')
   })
+
+  test('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('valid_email@email.com')
+    expect(account).toBeFalsy()
+  })
 })
