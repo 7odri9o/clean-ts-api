e9084adfb95f49d1446cd3320122c04afc9218fd
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-11-13 16:55:01 +0000

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -1,7 +1,7 @@
-import { Authentication, AuthenticationModel, HttpRequest, Validation } from './login-protocols'
+import { Authentication, AuthenticationModel, HttpRequest, Validation } from './login-controller-protocols'
 import { MissingParamError, ServerError } from '../../errors'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
-import { LoginController } from './login'
+import { LoginController } from './login-controller'
 
 const makeAuthenticator = (): Authentication => {
   class AuthenticatorStub implements Authentication {
