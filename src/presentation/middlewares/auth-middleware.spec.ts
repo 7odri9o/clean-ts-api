d86be73dfa9a71e77786d854327b045c821dc4c8
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-11-16 12:31:30 +0000

    refactor: move AuthMiddleware dependencies to a protocols file

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,9 +1,7 @@
 import { AuthMiddleware } from './auth-middleware'
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
-import { AccountModel } from '../../domain/models/account'
-import { HttpRequest } from '../protocols'
+import { AccountModel, HttpRequest, LoadAccountByToken } from './auth-middleware-protocols'
 
 const makeFakeRequest = (): HttpRequest => ({
   headers: {
