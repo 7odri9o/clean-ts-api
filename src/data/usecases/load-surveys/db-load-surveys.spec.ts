1ec0a277a050a5d943ecc9ecab70a0e04cb2e962
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-11-17 13:03:06 +0000

    test: ensure DbLoadSurveys return a surveys's list on success

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -1,3 +1,5 @@
+import MockDate from 'mockdate'
+
 import { SurveyModel } from '../../../domain/models/survey'
 import { DbLoadSurveys } from './db-load-surveys'
 import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
@@ -44,6 +46,14 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DbLoadSurveys', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('Should call LoadSurveysRepository', async () => {
     const { sut, loadSurveysRepositoryStub } = makeSut()
     const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
@@ -51,5 +61,13 @@ describe('DbLoadSurveys', () => {
     await sut.load()
 
     expect(loadAllSpy).toHaveBeenCalled()
+  })
+
+  test('Should return a surveys\'s list on success', async () => {
+    const { sut } = makeSut()
+
+    const surveys = await sut.load()
+
+    expect(surveys).toEqual(makeFakeSurveys())
   })
 })
