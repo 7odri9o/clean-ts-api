928d75c6112947cc52f5927ef639179d1137f92e
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-11-18 01:47:15 +0000

    test: ensure DbLoadSurveyById returns a survey on success

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -55,4 +55,12 @@ describe('DbLoadSurveyById Usecase', () => {
 
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
   })
+
+  test('Should return a survey on success', async () => {
+    const { sut } = makeSut()
+
+    const survey = await sut.loadById('any_id')
+
+    expect(survey).toEqual(makeFakeSurvey())
+  })
 })
