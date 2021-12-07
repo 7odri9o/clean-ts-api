905fe21b508e6cf4450bb105aceec753f061c1e1
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-12-07 17:39:05 -0300

    feat: apply no-cache middleware on swagger api

--- src/main/config/setup-swagger.ts
+++ src/main/config/setup-swagger.ts
@@ -1,7 +1,8 @@
 import { serve, setup } from 'swagger-ui-express'
 import { Express } from 'express'
 import swagger from '@/main/docs'
+import { noCache } from '@/main/middlewares/no-cache'
 
 export default (app: Express): void => {
-  app.use('/api-docs', serve, setup(swagger))
+  app.use('/api-docs', noCache, serve, setup(swagger))
 }
