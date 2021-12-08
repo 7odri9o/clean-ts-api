f5f3a6f38aa16b9fd199e875c3a37f8c0ec0c04c
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2021-12-08 12:59:52 -0300

    docs: add license to swagger

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -9,6 +9,10 @@ export default {
     description: 'API do Curso do Rodrigo Manguinho para realizar enquetes entre programadores',
     version: '1.0.0'
   },
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+  },
   servers: [
     {
       url: '/api'
